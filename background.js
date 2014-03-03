4bb50c6b55ae3796b95c21b4bc1ec1ebdaac3bd9
Cody M <mcmahon.t.cody@gmail.com>
2014-03-03 14:05:19 -0800

    Cycle
    Actually finds new tabs that haven't been seen instead of oscillating

--- background.js
+++ background.js
@@ -4,19 +4,30 @@
 //when searching from box, remove
 
 
-//helper function because concat does not do what I think it should do
+//no longer checks the only the current tab and stops after it hits name
+//will only oscillate
+//search forward from current position to fix
+//have some seen property
+
+//use index but can get stuck on a window
+
 chrome.omnibox.onInputStarted.addListener(function(){
     var all_tabs = [];
     var current_tab = {};
+    var before = []; //added to help with splice
     chrome.tabs.query({currentWindow:true, active:true}, function(tabs){
         current_tab = tabs[0];
+        //alert(current_tab.index);
         //alert(tabs[0].url);
     });
     chrome.tabs.query({currentWindow:true},function(tabs){
+        before = tabs.splice(0,current_tab.index)//grab before
+        tabs.splice(0,1)//remove the current tab
         all_tabs = all_tabs.concat(tabs);
     });
     chrome.tabs.query({currentWindow:false}, function(tabs){
         all_tabs = all_tabs.concat(tabs);
+        all_tabs = all_tabs.concat(before);//adds the tabs before current to the end so we cycle through everything
     });
     chrome.omnibox.onInputEntered.addListener(function(text,disposition){
         var len = text.length;
@@ -25,8 +36,12 @@ chrome.omnibox.onInputStarted.addListener(function(){
             for(var j = 0; j<=check.length-(len-1); j++){
                 if(check.substr(j, len) == text){
                     //move to this tab
-                    chrome.windows.update(all_tabs[i].windowId,{focused:true});
-                    chrome.tabs.update(all_tabs[i].id,{active:true});
+                    //alert(current_tab.url);
+                    //if(all_tabs[i].id != current_tab.id){
+                        chrome.windows.update(all_tabs[i].windowId,{focused:true});
+                        chrome.tabs.update(all_tabs[i].id,{active:true});
+                        return;
+                    //}
                 }
             }
         }
